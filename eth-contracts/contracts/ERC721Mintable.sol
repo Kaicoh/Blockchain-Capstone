3356e5c5bce02a907409bc7ab4ef46fc8e3f04b8
Kaicoh <sumireminami@gmail.com>
2019-05-24 18:04:31 +0900

    implement ERC721 contract

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -148,30 +148,35 @@ contract ERC721 is Pausable, ERC165 {
         _registerInterface(_INTERFACE_ID_ERC721);
     }
 
-    function balanceOf(address owner) public view returns (uint256) { // solium-disable-line no-empty-blocks
+    function balanceOf(address owner) public view returns (uint256) {
         // TODO return the token balance of given address
         // TIP: remember the functions to use for Counters. you can refresh yourself with the link above
+        return _ownedTokensCount[owner].current();
     }
 
-    function ownerOf(uint256 tokenId) public view returns (address) { // solium-disable-line no-empty-blocks
+    function ownerOf(uint256 tokenId) public view returns (address) {
         // TODO return the owner of the given tokenId
+        return _tokenOwner[tokenId];
     }
 
-//    @dev Approves another address to transfer the given token ID
-    function approve(address to, uint256 tokenId) public { // solium-disable-line no-empty-blocks
-
+    // @dev Approves another address to transfer the given token ID
+    function approve(address to, uint256 tokenId) public {
+        address tokenOwner = ownerOf(tokenId);
         // TODO require the given address to not be the owner of the tokenId
-
+        require(to != tokenOwner);
         // TODO require the msg sender to be the owner of the contract or isApprovedForAll() to be true
-
+        require(msg.sender == _owner || isApprovedForAll(tokenOwner, msg.sender));
         // TODO add 'to' address to token approvals
-
+        _tokenApprovals[tokenId] = to;
         // TODO emit Approval Event
-
+        emit Approval(tokenOwner, to, tokenId);
     }
 
-    function getApproved(uint256 tokenId) public view returns (address) { // solium-disable-line no-empty-blocks
+    function getApproved(uint256 tokenId) public view returns (address) {
         // TODO return token approval if it exists
+        address approval = _tokenApprovals[tokenId];
+        require(approval != address(0));
+        return approval;
     }
 
     /**
@@ -235,28 +240,31 @@ contract ERC721 is Pausable, ERC165 {
 
     // @dev Internal function to mint a new token
     // TIP: remember the functions to use for Counters. you can refresh yourself with the link above
-    function _mint(address to, uint256 tokenId) internal { // solium-disable-line no-empty-blocks
-
+    function _mint(address to, uint256 tokenId) internal {
         // TODO revert if given tokenId already exists or given address is invalid
-
+        require(!_exists(tokenId) && to != address(0));
         // TODO mint tokenId to given address & increase token count of owner
-
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to].increment();
         // TODO emit Transfer event
+        emit Transfer(address(0), to, tokenId);
     }
 
     // @dev Internal function to transfer ownership of a given token ID to another address.
     // TIP: remember the functions to use for Counters. you can refresh yourself with the link above
-    function _transferFrom(address from, address to, uint256 tokenId) internal { // solium-disable-line no-empty-blocks
-
+    function _transferFrom(address from, address to, uint256 tokenId) internal {
         // TODO: require from address is the owner of the given token
-
+        require(from == ownerOf(tokenId));
         // TODO: require token is being transfered to valid address
-
+        require(to != address(0));
         // TODO: clear approval
-
+        _clearApproval(tokenId);
         // TODO: update token counts & transfer ownership of the token ID
-
+        _ownedTokensCount[from].decrement();
+        _ownedTokensCount[to].increment();
+        _tokenOwner[tokenId] = to;
         // TODO: emit correct event
+        emit Transfer(from, to, tokenId);
     }
 
     /**
