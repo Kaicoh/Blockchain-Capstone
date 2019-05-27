f5d1ac4450007fe7177e81a5a61e5a27dbe84b0e
Kaicoh <sumireminami@gmail.com>
2019-05-27 19:36:08 +0900

    fix test description

--- eth-contracts/test/TestSquareVerifier.js
+++ eth-contracts/test/TestSquareVerifier.js
@@ -8,19 +8,21 @@ contract('SquareVerifier', () => {
         instance = await SquareVerifier.deployed();
     });
 
-    it('returns true when using correct proof', async () => {
-        const { proof: { a, b, c }, inputs } = proof;
-        const result = await instance.verifyTx.call(a, b, c, inputs);
-        assert.equal(result, true);
-    });
+    describe('verifyTx function', () => {
+        it('returns true when using correct proof', async () => {
+            const { proof: { a, b, c }, inputs } = proof;
+            const result = await instance.verifyTx.call(a, b, c, inputs);
+            assert.equal(result, true);
+        });
 
-    it('returns false when using incorrect proof', async () => {
-        const { proof: { a, b }, inputs } = proof;
-        const c = [
-            '0x0000000000000000000000000000000000000000000000000000000000000000',
-            '0x0000000000000000000000000000000000000000000000000000000000000000',
-        ];
-        const result = await instance.verifyTx.call(a, b, c, inputs);
-        assert.equal(result, false);
+        it('returns false when using incorrect proof', async () => {
+            const { proof: { a, b }, inputs } = proof;
+            const c = [
+                '0x0000000000000000000000000000000000000000000000000000000000000000',
+                '0x0000000000000000000000000000000000000000000000000000000000000000',
+            ];
+            const result = await instance.verifyTx.call(a, b, c, inputs);
+            assert.equal(result, false);
+        });
     });
 });
