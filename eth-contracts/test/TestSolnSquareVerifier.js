d7285064572e7ed698e346173543b59e1ee722a6
Kaicoh <sumireminami@gmail.com>
2019-05-27 23:11:25 +0900

    add SolnSquareVerifier test

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,34 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const truffleAssert = require('truffle-assertions'); // eslint-disable-line import/no-extraneous-dependencies
+const proof = require('../../zokrates/code/square/proof.json');
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract('SolnSquareVerifier', (accounts) => {
+    let instance;
+
+    beforeEach(async () => {
+        instance = await SolnSquareVerifier.deployed();
+    });
+
+    describe('mint function', () => {
+        it('adds new solution', async () => {
+            const { proof: { a, b, c }, inputs } = proof;
+            const tx = await instance.mint(a, b, c, inputs, { from: accounts[1] });
+            truffleAssert.eventEmitted(
+                tx,
+                'SolutionAdded',
+                event => event.account === accounts[1],
+            );
+        });
+
+        it('increments total supply', async () => {
+            const totalSupply = await instance.totalSupply.call();
+            assert.equal(totalSupply, 1);
+        });
+
+        it('assign new token to msg.sender', async () => {
+            const tokenId = 0; // first tokenId is 0
+            const owner = await instance.ownerOf.call(tokenId);
+            assert.equal(owner, accounts[1]);
+        });
+    });
+});
