7b3942a8575de9ee2d04acbf3b1214c143fcf2ee
Kaicoh <sumireminami@gmail.com>
2019-05-27 23:45:30 +0900

    deploy to rinkeby network

--- eth-contracts/truffle.js
+++ eth-contracts/truffle.js
@@ -1,9 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
+/* eslint-enable import/no-extraneous-dependencies */
+
+const { INFURA_KEY, MNEMONIC } = process.env;
+
+if (!INFURA_KEY || !MNEMONIC) {
+    throw new Error('Not found environment variables. INFURA_KEY or MNEMONIC.');
+}
+
 module.exports = {
     networks: {
         development: {
             host: '127.0.0.1',
             port: 8545,
             network_id: '*',
+        },
+
+        rinkeby: {
+            provider: () => new HDWalletProvider(
+                MNEMONIC,
+                `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+            ),
+            network_id: 4,
+            gas: 6500000,
+            gasPrice: 10000000000,
         },
     },
 
